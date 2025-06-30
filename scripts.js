c395b7219e63352db09b43934dcebf6403f3c897
NChoudhary16 <naveenchaudhary0002487@gmail.com>
2025-06-30 22:18:27 +0530

    asie hi

--- scripts.js
+++ scripts.js
@@ -1,7 +1,33 @@
-// gsap.to(".logo", {
-//   transformOrigin: "-500px 20px",
-//   x: 20,
-//   stagger: 0.2,
-//   duration: 0.5,
-//   opacity: 1,
-// });
+let tl = gsap.timeline();
+tl.from(".logo,.nav-links li", {
+  y: -50,
+  stagger: 0.2,
+  duration: 0.5,
+  opacity: 0,
+});
+tl.from(".login-btn", {
+  y: 50,
+  rotate: 720,
+  stagger: 0.2,
+  duration: 0.5,
+  opacity: 0,
+});
+tl.from(".cart-btn", {
+  x: -500,
+  rotate: 1440,
+  stagger: 0.2,
+  duration: 0.5,
+  opacity: 0,
+});
+tl.from(".txt-area h2", {
+  x: 300,
+  opacity: 0,
+  scale: 1.1,
+  stagger: 0.3,
+});
+tl.from(".rocket", {
+  y: 300,
+  opacity: 0,
+  scale: 1.1,
+  stagger: 0.3,
+});
